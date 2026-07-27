5c164e009f4e58637f120079f0a1dda8a3d08cfe
Upendra Polisetty <polisettyupendra@gail.com>
2026-07-27 22:08:52 +0530

    DataTypes

---
+++ DataTypes/ExampleTypes.py
@@ -0,0 +1,9 @@
+name = "polisetty_lakshman"
+Age = 25
+Hight = 5.9
+is_employe = True
+#to check the data types of the variables we can use type () 
+print(type(name))
+print(type(Age))
+print(type(Hight))
+print(type(is_employe))
--- variables/MiniChallenge.py
+++ variables/MiniChallenge.py
@@ -1,7 +1,10 @@
-Name = " lakshman"
-Age =  25
-city =" Tirupati"
+Name = "Lakshman"
+Age = 25
+city ="Tirupati"
 
+
+# To print the values of the variables, you can use the print function in Python.
 print( Name)
 print( Age)
 print( city)
+
