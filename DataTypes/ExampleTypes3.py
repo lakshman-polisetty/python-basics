e9e843192c4d3a1be5a25c01b7e3af4b41b0c62c
Upendra Polisetty <polisettyupendra@gail.com>
2026-07-29 12:08:59 +0530

    Datatype sample code

---
+++ DataTypes/ExampleTypes3.py
@@ -0,0 +1,17 @@
+name = "lakshman polisetty"
+age = 25
+height = 5.8
+college = "Svce"
+is_student = True
+# to print the data types of the types of the variables
+print(type(name))
+print(type(age))
+print(type(height))
+print(type(college))
+print(type(is_student))
+
+print("name:", name)
+print("age:", age)
+print("height:", height)
+print("college:", college)
+print("is_student:", is_student)
