cfb28940ba9944be9fca2bab5075b029d1b8b4ec
Upendra Polisetty <polisettyupendra@gail.com>
2026-08-04 11:12:12 +0530

    Logical Operators

---
+++ OperatorsTypes/LogicalOperators/AND.py
@@ -0,0 +1,11 @@
+pen_price = 50
+book_price = 40
+
+box_price = 50
+bottle_price = 70
+# Operator	                        Meaning
+#  and	                  Both conditions must be True
+
+print(pen_price > book_price and box_price > bottle_price)  
+print(50 > 40 and 50 > 40)
+print(10 > 5 and 20 < 10)
