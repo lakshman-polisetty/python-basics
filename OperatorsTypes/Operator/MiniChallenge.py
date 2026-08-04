c0bd6cd4704e189f3e1a530519d2096cf86da3b5
Upendra Polisetty <polisettyupendra@gail.com>
2026-08-04 12:05:34 +0530

    Mini Challenge about Operators

---
+++ OperatorsTypes/Operator/MiniChallenge.py
@@ -0,0 +1,34 @@
+a = 25
+b = 10
+# Addition
+print (a + b)
+
+a = 25
+b = 10
+# Subtraction 
+print (a -b)
+
+a = 25
+b = 10
+# Multiplication results in integer
+print (a * b)
+
+a = 25
+b = 10
+# Division reslts in float
+print (a / b)
+
+a = 25
+b = 10
+# Modulus result shows the remainder after division
+print (a % b)
+
+a = 25
+b = 10
+# Floor division results
+print (a // b)
+
+a = 25
+b = 10
+# Exponentiation results in power
+print (a ** 2)
